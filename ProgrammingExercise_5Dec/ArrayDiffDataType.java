4ba02821075030dd3c111af5314c643c9ffe76aa
FarisahHannes2210 <Hannesfarisah@gmail.com>
2025-01-14 03:52:44 +0800

    week 3

---
+++ ProgrammingExercise_5Dec/ArrayDiffDataType.java
@@ -0,0 +1,13 @@
+package Week3.ProgrammingExercise_5Dec;
+
+ class ArrayDiffDataType {
+     /// Create Arrays for Different Data Types: Write a Java program that declares and initialises
+     /// arrays for the following data types without explicitly setting their values:
+     /// 1. int
+     /// 2. double
+     /// 3. char
+     /// 4. boolean
+     /// 5. String
+
+     int[] intArray;
+}
